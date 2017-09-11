d5f508645367c4cd56756734d18f46a0f45d4d19
Daniel Varvara <varvara_sd@yahoo.co.uk>
2017-09-11 19:14:44 +0300

    use nested routes and style with activeStyle

--- lessons/01-setting-up/modules/App.js
+++ lessons/01-setting-up/modules/App.js
@@ -1,10 +1,17 @@
 import React from 'react'
+import {Link} from 'react-router'
+
 
 export default React.createClass({
   render() {
     return (
     <div>
       <h1>Hello, React Router!</h1>
+      <ul role="nav">
+        <li><Link to="/about" activeStyle={{"color":"red"}}>About</Link></li>
+        <li><Link to="/repos" activeStyle={{"color":"green"}}>Repos</Link></li>
+      </ul>
+      {this.props.children}
     </div>
     )
   }
